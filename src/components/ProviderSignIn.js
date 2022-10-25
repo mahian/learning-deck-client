bd0b7c880d78b9e797beccdf5f1d2a88347d51d1
mahian ahmad <mahianahamd9@gmail.com>
2022-10-26 00:21:26 +0600

    style provider login

--- src/components/ProviderSignIn.js
+++ src/components/ProviderSignIn.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ProviderSignIn = () => {
     return (
-        <div>
-            <button><img src="assets/google.png" alt="google" /></button>
+        <div className='flex justify-center'>
+            <button className='w-10 h-10 shadow-md p-1 rounded-lg mx-2'><img className='w-full' src="assets/google.png" alt="google" /></button>
+            <button className='w-10 h-10 shadow-md p-1 rounded-lg mx-2'><img className='w-full' src="assets/facebook.png" alt="google" /></button>
+            <button className='w-10 h-10 shadow-md p-1 rounded-lg mx-2'><img className='w-full' src="assets/github.png" alt="google" /></button>
         </div>
     );
 };
